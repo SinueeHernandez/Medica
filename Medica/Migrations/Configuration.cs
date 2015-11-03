c20f0080e5daf2e1bba25cc5c8016dd8a972f0fc
SinueeHernandez <esteban.hernandez@definityfirst.com>
2015-11-03 17:08:22 -0600

    Registered users can edit their consults

--- Medica/Migrations/Configuration.cs
+++ Medica/Migrations/Configuration.cs
@@ -4,6 +4,8 @@ namespace Medica.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Medica.Models.ApplicationDbContext>
     {
@@ -19,6 +21,16 @@ namespace Medica.Migrations
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
             //
+            AddUserAndRole(context);
+        }
+
+        bool AddUserAndRole(Medica.Models.ApplicationDbContext context)
+        {
+            IdentityResult ir;
+            var rm = new RoleManager<IdentityRole>
+                (new RoleStore<IdentityRole>(context));
+            ir = rm.Create(new IdentityRole("canUpdateCatalogs"));
+            return ir.Succeeded;
         }
     }
 }
